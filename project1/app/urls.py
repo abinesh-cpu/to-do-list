36f66638714a75fa8c38cf8cb8cffc155d2d9092
abinesh-cpu <abineshrl476@gmail.com>
2024-12-09 13:31:22 +0530

    todolist work

--- project1/app/urls.py
+++ project1/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('',views.todolist1),
     path('todo_update/<pk>',views.todo_update),
+    path('todo_delete/<pk>',views.todo_delete),
 ]
--- project1/app/views.py
+++ project1/app/views.py
@@ -10,9 +10,18 @@ def todolist1(request):
         todoarea=request.POST['todoarea']
         data=todo.objects.create(sub=todoarea)
         data.save()
-    return render(request,'index.html',{'todods':todos})
+    return render(request,'index.html',{'todos':todos})
 
 def todo_update(request,pk):
-    data=todo.objects.get(pk=pk)
-    print(data.sub)
-    return render(request,'update.html',{'data':data})
+    todos=todo.objects.all()
+    if request.method=='POST':
+        todoarea=request.POST['todoarea']
+        todo.objects.filter(pk=pk).update(sub=todoarea)
+        return redirect(todolist1)
+    else:
+        data=todo.objects.get(pk=pk)
+        print(data.sub)
+    return render(request,'index.html',{'data':data,'todos':todos})
+def todo_delete(request,pk):
+    todo.objects.filter(pk=pk).delete()
+    return redirect(todolist1)
